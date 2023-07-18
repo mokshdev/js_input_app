08d0bb5aebc43a5e1d546d516f9df45622c829d0
mokshdev <mokshsingh12345@gmal.com>
2023-07-18 22:43:28 +0530

    concept for adding date and printing it

--- practice/arrrayobj.js
+++ practice/arrrayobj.js
@@ -1,17 +1,37 @@
 let array = []
 
 const getElements = (name,age)=>{
-    console.log({name,age});
+    // console.log({name,age});
     array.push({name,age})
-    console.log(array);
+    // console.log(array);
     return{
         name,age
     }
 }
 
 getElements("moksh",12)
-console.log(array[0].name);
-if (array[0].age == 12) {
-    array[0].age = 18
-    console.log(array);
+getElements("hitika",15)
+getElements("kartik",210)
+// console.log(array[0].name);
+// if (array[0].age == 12) {
+//     array[0].age = 18
+//     console.log(array);
+// }
+
+
+
+for (let i = 0; i < array.length; i++) {
+    const name = array[i].name;
+    const age = array[i].age
+    console.log(array[i].name);
+    let main = document.querySelector(".main")
+    const div = document.createElement("div")
+    div.classList.add('blocks')
+    main.appendChild(div)
+    div.textContent =  `name: ${array[i].name}, age of ${name} is: ${age}`
+    
+    
+    
 }
+
+console.log(array);
