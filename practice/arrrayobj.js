87f1b5ec07d56a7ace86e0a9f0a26f9a54ffc15f
mokshdev <mokshsingh12345@gmal.com>
2023-07-24 13:23:36 +0530

    projeect add and showing todo acc to project added

--- practice/arrrayobj.js
+++ practice/arrrayobj.js
@@ -1,97 +1,52 @@
+import { modifybutton } from "./modifybutton.js";
+import { addProjectToDOM } from "./project.js";
+//initialisation
+//creating the array
 let array = [];
 
-
-const getElements = (name, age) => {
-  array.push({ name, age });
+//getting the list of array
+const getElements = (name, age,projectNeo) => {
+  array.push({ name, age ,projectNeo});
 };
 
-getElements("moksh", 12);
-getElements("hitika", 15);
-getElements("kartik", 210);
-
-const modifybutton = (inputName) =>{
-const main = document.querySelector(".main");
-const form = document.createElement("form");
-const formDiv0 = document.createElement("div");
-const label = document.createElement("label");
-const input = document.createElement("input")
-const submit = document.createElement("button")
-
-main.appendChild(form);
-form.classList.add("form");
-form.appendChild(formDiv0);
+//adding data
+getElements("moksh", 12,"main");
+getElements("hitika", 15,"siblings");
+getElements("kartik", 210,"siblings");
 
 
-// label 
-formDiv0.appendChild(label);
-label.textContent = "Name:";
-label.for = "name"
-
-// input 
-formDiv0.appendChild(input)
-input.type = "text"
-input.id = "name"
-input.name = "name"
-input.required = "true"
-
-// submit button
-submit.textContent = "submit"
-form.appendChild(submit)
-submit.type = "submit"
-submit.addEventListener("click",function (e) {
-    e.preventDefault()
-    console.log(input.value);    
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].name === inputName) {
-            array[i].name = `${input.value}`
-        }
-    }
-    main.innerHTML = "";
-    for (let i = 0; i < array.length; i++) {
-        const name = array[i].name;
-        const age = array[i].age;
-    
-        const div = document.createElement("div");
-        div.classList.add("blocks");
-        div.textContent = `name: ${name}, age of ${name} is: ${age}`;
-        main.appendChild(div);
-    
-        const button = document.createElement("button");
-        button.classList.add("button");
-        button.innerText = name;
-        button.value = name;
-        button.id = name;
-        main.appendChild(button);
-      }
-console.log(array);
-})
-}
 
 
 const addButtonToDOM = () => {
   let main = document.querySelector(".main");
-  main.innerHTML = "";
+  main.innerHTML = "";//providing preprovided data
 
-    
-
+  
+    //displaying avaialable data
   for (let i = 0; i < array.length; i++) {
     const name = array[i].name;
     const age = array[i].age;
-    modifybutton(name)
+    const projectName = array[i].projectNeo
+    modifybutton(name,array)//add comment here
+    
     const div = document.createElement("div");
     div.classList.add("blocks");
-    div.textContent = `name: ${name}, age of ${name} is: ${age}`;
+    div.textContent = `name: ${name}, age of ${name} is: ${age} and the projec is: ${projectName}` ;
     main.appendChild(div);
 
     const button = document.createElement("button");
     button.classList.add("button");
-    button.innerText = name;
+    button.innerText = `delete: ${name}`;
     button.value = name;
     button.id = name;
     main.appendChild(button);
+    DeleteThings()
   }
 
-  const delButtons = document.querySelectorAll(".button");
+  
+};
+export const DeleteThings =()=>{
+    const delButtons = document.querySelectorAll(".button");
   delButtons.forEach(function (delButton) {
     delButton.addEventListener("click", function (e) {
       e.preventDefault();
@@ -101,18 +56,19 @@ const addButtonToDOM = () => {
       
     });
   });
-};
-
-const removeName = (delName) => {
-  const lowName = delName.toLowerCase();
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].name.toLowerCase() === lowName) {
-      array.splice(i, 1);
-      break;
+  const removeName = (delName) => {
+    const lowName = delName.toLowerCase();
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].name.toLowerCase() === lowName) {
+        array.splice(i, 1);
+        break;
+      }
     }
-  }
-};
+  
+  };
+}
+DeleteThings()
 
 addButtonToDOM(); // Call the function initially to populate the DOM
 let modiTxtBtn = document.querySelector(".modi-txt-btn")
@@ -121,50 +77,7 @@ modiTxtBtn.addEventListener("click",function (e) {
     addButtonToDOM()
 })
 
-// creating form
-/*const main = document.querySelector(".main");
-const form = document.createElement("form");
-const formDiv0 = document.createElement("div");
-const label = document.createElement("label");
-const input = document.createElement("input")
-const submit = document.createElement("button")
-
-main.appendChild(form);
-form.classList.add("form");
-form.appendChild(formDiv0);
-
-
-// label 
-formDiv0.appendChild(label);
-label.textContent = "Name:";
-label.for = "name"
-
-// input 
-formDiv0.appendChild(input)
-input.type = "text"
-input.id = "name"
-input.name = "name"
-input.required = "true"
-
-// submit button
-submit.textContent = "submit"
-form.appendChild(submit)
-submit.type = "submit"
-
-
-
-// event listener on submit of form
-submit.addEventListener("click",function (e) {
-    e.preventDefault()
-    console.log(input.value);    
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].name === "moksh") {
-            array[i].name = `${input.value}`
-        }
-    }
-    
-console.log(array);
-})*/
+addProjectToDOM()
 
 
 
@@ -173,5 +86,5 @@ console.log(array);
 
 // console.log(array);
 
-/*so i will use diff div in side the alreeadey present div, or i can add a value to the div and then i will use that to get the valye and then i will use that value to updat ethe array , in order the modify i will do the same instead of removing i will updat the value in the array, also i wil ladd an elemant name project in the array, defauly will be all and wwhen added customly it will have it modified, and then when we click on the name of project, the function will search thaqt array in the array of todo and display the sorted data in the column, same with priority and date also. 
+/*so i will use diff div in side the alreeadey present div, or i can add a value to the div and then i will use that to get the valye and then i will use that value to updat ethe array , in order the modify i will do the same instead of removing i will updat the value in the array, also i will add an elemant name project in the array, default will be all and wwhen added customly it will have it modified, and then when we click on the name of project, the function will search thaqt array in the array of todo and display the sorted data in the column, same with priority and date also. 
 once i am done with all of this basic knowledge i will save the data locally and fetch it from there. thats it and it will work as intended */
---
+++ practice/project.js
@@ -0,0 +1,145 @@
+let project = []
+
+let array = []
+const getElements = (name, age,projectNeo) => {
+    array.push({ name, age ,projectNeo});
+  };
+getElements("moksh", 12,"main");
+getElements("hitika", 15,"siblings");
+getElements("kartik", 210,"siblings"); 
+
+
+
+
+
+
+const getProjects = (pName) => {
+    project.push(pName);
+    console.log(project);
+  }
+
+
+  getProjects("main")
+  getProjects("siblings")
+
+
+
+export  const addProjectToDOM = () => {
+    let projects = document.querySelector(".projects");
+    projects.innerHTML = "";//providing preprovided data
+  
+    
+      //displaying avaialable data
+    for (let i = 0; i < project.length; i++) {
+      const name = project[i];
+      
+      const div = document.createElement("div");
+      div.classList.add("project-details");
+      div.textContent = `name: ${name}`;
+      projects.appendChild(div);
+  
+      const button = document.createElement("button");
+      button.classList.add("button");
+      button.innerText = `delete: ${name}`;
+      button.value = name;
+      button.id = name;
+      projects.appendChild(button);
+
+        //button for showing todo with projectnames lister
+        
+    
+        const showBtn = document.createElement("button");
+        showBtn.innerText = `show items of: ${name}`;
+        showBtn.value = name;
+        showBtn.id = "project-details";
+        projects.appendChild(showBtn);
+
+
+        // Add an event listener to each "project-details" button
+        showBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log(showBtn.value); // Log the value of the clicked "project-details" button
+        // Perform any other actions related to the "project-details" button here
+
+    
+            let main = document.querySelector(".main");
+            main.innerHTML = "";//providing preprovided data
+          
+            
+              //displaying avaialable data
+            for (let i = 0; i < array.length; i++) {
+              const name = array[i].name;
+              const age = array[i].age;
+              const projectName = array[i].projectNeo
+              
+              if (projectName === showBtn.value) {
+                const div = document.createElement("div");
+                div.classList.add("blocks");
+                div.textContent = `name: ${name}, age of ${name} is: ${age} and the projec is: ${projectName}` ;
+                main.appendChild(div);
+            
+                const button = document.createElement("button");
+                button.classList.add("button");
+                button.innerText = `delete: ${name}`;
+                button.value = name;
+                button.id = name;
+                main.appendChild(button); 
+              }
+              
+              
+
+          
+            
+          };
+      })
+
+
+      delteProjects()
+    }
+    
+    
+  };
+
+
+
+
+
+
+  const delteProjects =()=>{
+    
+      const delButtons = document.querySelectorAll(".button");
+    delButtons.forEach(function (delButton)/*to select every button with .button id*/ {
+      delButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        removeName(`${delButton.value}`);
+        console.log(project);
+        addProjectToDOM(); // Call the function recursively to update the DOM
+        
+      });
+    });
+
+
+
+    const removeName = (delName) => {
+      const lowName = delName.toLowerCase();
+    
+      for (let i = 0; i < project.length; i++) {
+        if (project[i].toLowerCase() === lowName) {
+          project.splice(i, 1);              
+          break;
+        }
+      }
+    
+    };
+  }
+
+  
+  addProjectToDOM(); // Call the function initially to populate the DOM
+  let modiTxtBtn = document.querySelector(".modi-txt-btn")
+  modiTxtBtn.addEventListener("click",function (e) {
+      e.preventDefault()
+      addProjectToDOM()
+  })
+
+
+  // create a selecting option
